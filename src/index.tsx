cb0f5d8334bf91bdb5dc7a211ac3d5df27736330
lucas-oliveira-viana <lucas_oliveira_viana@hotmail.com>
2023-11-06 22:47:19 -0300

    change to sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import "./styles.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,8 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.svg$/,
